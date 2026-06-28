18924e573bbf74bef18cdebaa5a44e18a4b249d4
Aakriti2728 <chandanaakriti05@gmail.com>
2026-06-28 22:19:28 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "Which of the following is NOT a type of network topology?",
+    option1: "Bus",
+    option2: "Star",
+    option3: "Ring",
+    option4: "Bridge",
+    ans: 4
+  },
+  {
+    question: "What is the main function of the transport layer in the OSI model?",
+    option1: "Routing data across networks",
+    option2: "Encoding and decoding data",
+    option3: "Ensuring reliable data transfer",
+    option4: "Assigning IP addresses",
+    ans: 3
+  },
+  {
+    question: "Which protocol is used to assign IP addresses dynamically to devices on a network?",
+    option1: "DNS",
+    option2: "HTTP",
+    option3: "DHCP",
+    option4: "FTP",
+    ans: 3
+  },
+  {
+    question: "In which type of network are all computers considered equal and share resources without a central server?",
+    option1: "Client-server",
+    option2: "Peer-to-peer",
+    option3: "Cloud network",
+    option4: "Metropolitan area network",
+    ans: 2
+  },
+  {
+    question: "Which device is used to connect different networks together?",
+    option1: "Switch",
+    option2: "Router",
+    option3: "Hub",
+    option4: "Repeater",
+    ans: 2
+  }
+];
